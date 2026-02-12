174b6d67b04ca638b81a3ebb15ab8049947ca460
Kevinliao0857 <kevinliao0857@gmail.com>
2026-02-11 21:58:23 -0800

    init update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,14 @@
 from .config import EST, TRADER
 from .filters import is_crypto, get_up_down
-from .api import track_0x8dxd
+from .api import (
+    track_0x8dxd, get_latest_bets, 
+    get_profile_name, get_trader_pnl, 
+    get_open_positions, get_closed_trades_pnl
+)
 
-__all__ = ['track_0x8dxd', 'EST', 'TRADER', 'is_crypto', 'get_up_down']
+__all__ = [
+    'EST', 'TRADER', 'is_crypto', 'get_up_down',
+    'track_0x8dxd', 'get_latest_bets', 
+    'get_profile_name', 'get_trader_pnl', 
+    'get_open_positions', 'get_closed_trades_pnl'
+]
